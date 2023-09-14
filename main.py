2d58cd85254c9790e777d9bb68b6632d2791380d
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-14 14:44:19 -0700

    commit c3

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ async def index():
     return {"Data":"Blog List"}
 
 
+@app.get('/blog/{id}')
+async def get_blog(id:int):
+    return {"Data":id}
+
+
 @app.get('/about')
 async def about():
     return {"data":"This is About Page"}
+
+@app.get('/blog/{id}/comments')
+async def get_comments(id:int):
+    return {"data":{"1","2"}}
