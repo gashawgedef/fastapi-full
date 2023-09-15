3e8af37c8e4a4f35b0a3458c90472efd7d84a951
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-15 14:18:10 -0700

    gashity bacjkend 4

--- Blog/main.py
+++ Blog/main.py
@@ -1,24 +1,39 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine,SessionLocal
+from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-app=FastAPI()
+
+app = FastAPI()
 
 
 models.Base.metadata.create_all(engine)
 
+
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
 
-@app.post('/blog')
-def create(request:schemas.Blog,db:Session=Depends(get_db)):
-    new_blog=models.Blog(title=request.title,body=request.body)
+@app.post("/blog")
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get("/blog")
+def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get("/blog/{id}")
+def show(id,db: Session = Depends(get_db)):
+    data=db.query(models.Blog).filter(models.Blog.id==id).first()
+    return data
+
