611081aff397b4a735941d3650bf573a4cd8fc91
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-18 13:54:58 -0700

    gashaw gedef shibabaw

--- Blog/models.py
+++ Blog/models.py
@@ -1,18 +1,23 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship ,Mapped,mapped_column
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 
 from .database import Base
+from sqlalchemy.orm import relationship
 
 
 class Blog(Base):
     __tablename__ = "blogs"
     id = Column(Integer, primary_key=True, index=True)
-    title:  Mapped[str] = mapped_column()
-    body:  Mapped[str] = mapped_column()
+    title: Mapped[str] = mapped_column()
+    body: Mapped[str] = mapped_column()
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    creator = relationship("User", back_populates="blogs")
+
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    name:  Mapped[str] = mapped_column()
-    email:  Mapped[str] = mapped_column()
-    password:  Mapped[str] = mapped_column()
+    name: Mapped[str] = mapped_column()
+    email: Mapped[str] = mapped_column()
+    password: Mapped[str] = mapped_column()
+    blogs = relationship("Blog", back_populates="creator")
--- Blog/schemas.py
+++ Blog/schemas.py
@@ -1,4 +1,11 @@
 from pydantic import BaseModel
+from typing import List
+
+
+class User(BaseModel):
+   name:str
+   email:str
+   password:str
 
 class Blog(BaseModel):
    
@@ -6,17 +13,18 @@ class Blog(BaseModel):
    body:str
 
 
-class ShowBlog(Blog):
-   class Config():
-      orm_mode=True
-
-class User(BaseModel):
-   name:str
-   email:str
-   password:str
-
 class ShowUser(BaseModel):
    name:str
    email:str
+   blogs:List[Blog]=[]
+   class Config():
+      orm_mode=True
+
+
+
+class ShowBlog(BaseModel):
+   title:str
+   body:str
+   creator:ShowUser
    class Config():
       orm_mode=True
