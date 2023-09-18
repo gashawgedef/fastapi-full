d58e76e895a007a6fa11f5aaaa5b65b4fcec7b71
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-18 16:05:40 -0700

    gashaw gedef

--- Blog/routers/blog.py
+++ Blog/routers/blog.py
@@ -1,41 +1,33 @@
-from fastapi import APIRouter, Depends, HTTPException, Response, status, requests
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
-from .. import schemas, database, models
+from .. import schemas, database
 from ..database import get_db
 from typing import List
 from ..repository import blog
 
-router = APIRouter(
-    prefix='/blog',
-    tags=["blogs"]
-    
-    
-    )
+router = APIRouter(prefix="/blog", tags=["blogs"])
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(database.get_db)):
-    return blog.create_new_blogs(request,db)
- 
+    return blog.create_new_blogs(request, db)
 
 
 @router.get("/", response_model=List[schemas.ShowBlog])
 def get_all(db: Session = Depends(get_db)):
-   return blog.get_all_blogs(db)
+    return blog.get_all_blogs(db)
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_item(id, db: Session = Depends(get_db)):
-   return blog.delete_blog(id,db)
+    return blog.delete_blog(id, db)
 
 
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update_item(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    return blog.update_blog(id,request,db)
-
+    return blog.update_blog(id, request, db)
 
 
 @router.get("/{id}", status_code=200, response_model=schemas.ShowBlog)
-def show(id, response: Response, db: Session = Depends(get_db)):
-    return blog.get_single_blog(id,db)
- 
+def show(id, db: Session = Depends(get_db)):
+    return blog.get_single_blog(id, db)
--- Blog/routers/user.py
+++ Blog/routers/user.py
@@ -1,20 +1,19 @@
-from fastapi import APIRouter, Depends, HTTPException, Response, status, requests
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from .. import schemas, database, models
+from .. import schemas
 from ..database import get_db
-from typing import List
+from ..repository import user
 
-from .. repository import user
+
+
 router = APIRouter(prefix="/user", tags=["users"])
 
 
 @router.post("/", response_model=schemas.ShowUser, tags=["users"])
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    return user.create_new_user(request,db)
-    # hashed_password=pwd_cxt.hash(request.password)
- 
+    return user.create_new_user(request, db)
 
-# get a single user
+
 @router.get("/{id}", response_model=schemas.ShowUser, tags=["users"])
 def get_user(id: int, db: Session = Depends(get_db)):
-    return user.get_single_user(id,db)
+    return user.get_single_user(id, db)
