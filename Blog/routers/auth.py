48ec2887bd4696a12e77cb818d618676b879afb2
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-18 16:40:07 -0700

    commit this item

--- Blog/routers/auth.py
+++ Blog/routers/auth.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 
-router=APIRouter()
+from .. import models
 
-@router.post('/login')
-def login():
-    return "Login"
+from .. import schemas, database
+from sqlalchemy.orm import Session
+
+router = APIRouter(prefix="/login", tags=["Authentication"])
+
+
+@router.post("/")
+def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == request.username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Invalid credentials",
+        )
+    return user
